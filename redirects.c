17d163543a9414165507d1af952b536c14351aaa
student <student@iMac-student.local>
2020-12-29 17:23:38 +0300

    multiply redirects implimented

--- redirects.c
+++ redirects.c
@@ -3,18 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   redirects.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lbagg <lbagg@student.42.fr>                +#+  +:+       +#+        */
+/*   By: student <student@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/21 13:00:49 by lbagg             #+#    #+#             */
-/*   Updated: 2020/12/28 14:22:06 by lbagg            ###   ########.fr       */
+/*   Updated: 2020/12/29 17:00:07 by student          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		open_flags(t_command *cmds)
+int		open_flags(t_command *cmds, int std)
 {
-	if (cmds->out_fname)
+	if (std == 1)
 	{
 		if (!cmds->append)
 			return(open(cmds->out_fname, O_CREAT | O_WRONLY | O_TRUNC, 0777));
@@ -24,36 +24,43 @@ int		open_flags(t_command *cmds)
 	return(open(cmds->in_fname, O_RDONLY));
 }
 
+int		fd_dup(int fd_std, int file, int *fd_linkstd)
+{
+	int	fd_tmp;
+
+	*fd_linkstd = dup(fd_std);
+	fd_tmp = dup2(file, fd_std);
+	if (close(file) == -1 || fd_tmp == -1)
+		return (-1);
+	return (fd_tmp);
+}
+
 void	execute_redirects(char **args, t_command *cmds, t_all *all)
 {
-	int fd_file;
-	int	fd_tmp;
-	int	fd_old;
+	int fd_filein;
+	int fd_fileout;
+	int	fd_stdin;
+	int	fd_stdout;
 		
-	if ((fd_file = open_flags(cmds)) == -1)
-	{
-		error(ER_FILE);
-		return ;
-	}
 	if (cmds->out_fname)
 	{
-		fd_old = dup(STDOUT_FILENO);
-		fd_tmp = dup2(fd_file, STDOUT_FILENO);
+		if ((fd_fileout = open_flags(cmds, 1)) == -1)
+			return (error(ER_FILE));
+		if ((fd_fileout = fd_dup(STDOUT_FILENO, fd_fileout, &fd_stdout)) == -1)
+			return (error(ER_FILE));
 	}
-	else
+	if (cmds->in_fname)
 	{
-		fd_old = dup(STDIN_FILENO);
-		fd_tmp = dup2(fd_file, STDIN_FILENO);
-	}
-	if (close(fd_file) == -1 || fd_tmp == -1)
-	{
-		error(ER_FILE);
-		return ;
+		if ((fd_filein = open_flags(cmds, 0)) == -1)
+			return (error(ER_FILE));
+		if ((fd_filein = fd_dup(STDIN_FILENO, fd_filein, &fd_stdin)) == -1)
+			return (error(ER_FILE));
 	}
 	execute(args, all);
-	if (dup2(fd_old, fd_tmp) == -1 || close(fd_old) == -1)
-	{
-		error(ER_FILE);
-		return ;
-	}
+	if (cmds->out_fname)
+		if (dup2(fd_stdout, fd_fileout) == -1 || close(fd_stdout) == -1)
+			return (error(ER_FILE));
+	if (cmds->in_fname)
+		if (dup2(fd_stdin, fd_filein) == -1 || close(fd_stdin) == -1)
+			return (error(ER_FILE));
 }
